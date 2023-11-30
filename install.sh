9c91805824e54b325b320c34ab6903d0e8001223
Brendan Hansen <brendan.f.hansen@gmail.com>
2023-11-29 21:13:11 -0600

    changed where lsp config is stored; fixed installation

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 
 set -o pipefail
 
-pushd ..
-onyx pkg sync
-popd
-
-onyx build ../build -o onyx-lsp.wasm -V
+onyx package sync
+onyx build build.onyx -o onyx-lsp.wasm -V
 
 mkdir -p $ONYX_PATH/tools
 cp ./onyx-lsp.wasm $ONYX_PATH/tools/lsp.wasm
